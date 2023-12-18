ef00a39f05f47abbd1bf404f81831b08288b7598
Lakshya Agarwal <Lakshya.Agarwal@lntinfotech.com>
2023-12-18 17:29:51 +0530

    dependencies and properties upfated, all wotking on tls

--- shortener/src/main/java/com/lakshya/shortener/entity/MyEntity.java
+++ shortener/src/main/java/com/lakshya/shortener/entity/MyEntity.java
@@ -1,11 +1,11 @@
 package com.lakshya.shortener.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "my_entity")
@@ -16,6 +16,7 @@ public class MyEntity {
 	private Long id;
 
 	@Column(name = "name")
+	
 	private String name;
 
 	@Column(name = "description")
